e7772a8c8c9691bfdf7d4646804dc5ade12f418f
idiot2222 <pky2892@gmail.com>
2022-07-26 00:46:08 +0900

    add basic layout

---
+++ src/layouts/AppBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app-bar
+  app
+  color="#74b9ff"
+  dark
+  >
+  <div class="d-flex align-center">
+    <h1>Yajalal</h1>
+  </div>
+
+  <v-spacer></v-spacer>
+
+  <v-btn color="#0984e3">
+    Log in
+    <v-icon small>mdi-login</v-icon>
+  </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+    name: "AppBar",
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+    },
+    components: {
+    },
+}
+</script>
+
+<style>
+</style>
