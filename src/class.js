f85373b366b07059095143f36c243539d8e09a86
idiot2222 <pky2892@gmail.com>
2022-10-29 21:43:35 +0900

    add class.js
    클래스들 따로 모아둠

---
+++ src/class.js
@@ -0,0 +1,27 @@
+export class Pitcher {
+    constructor() {
+        this.player = '';
+        this.ip = 0;
+        this.k = 0;
+        this.bb = 0;
+        this.er = 0;
+        this.decision = 'NO';
+    }
+}
+
+export class Batter {
+    constructor() {
+        this.player = '';
+        this.ab = 0;
+        this.h = 0;
+        this.h2 = 0;
+        this.h3 = 0;
+        this.hr = 0;
+        this.bb = 0;
+        this.rbi = 0;
+        this.r = 0;
+        this.sb = 0;
+        this.cs = 0;
+        this.k = 0;
+    }
+}
